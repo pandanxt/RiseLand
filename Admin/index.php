fb9f787ef4875b9042ef2efb4caf65b2b38f00bc
mobeen887 <41565155+mobeen887@users.noreply.github.com>
2021-01-31 00:36:24 +0500

    Admin Panel Include folder and files are created

---
+++ Admin/index.php
@@ -0,0 +1,9 @@
+<?php include ('include/header.php'); ?>
+  <!-- Left side column. contains the logo and sidebar -->
+  <?php include ('include/sidebar.php'); ?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <?php include ('include/main.php') ?>
+
+  <!-- Main Footer -->
+  <?php include ('include/footer.php'); ?>
