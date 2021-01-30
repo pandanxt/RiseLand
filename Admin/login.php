a0d70b32a0d577eb1fcf07f6e87560bd3f20d0ed
mobeen887 <41565155+mobeen887@users.noreply.github.com>
2021-01-31 03:14:49 +0500

    Admin Panel Title Is changed.

--- Admin/login.php
+++ Admin/login.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Admin Panel</title>
+  <link rel="icon" href="../img/Logo.jpeg" type="image" sizes="16x16"><title>Rise Land</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
